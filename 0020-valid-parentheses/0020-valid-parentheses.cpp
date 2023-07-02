a83ceecf853f1b5cb583ed11e7fe0ee1a25069f3
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-07-02 16:47:37 -0400

    Time: 4 ms (40.18%), Space: 6.3 MB (38.74%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size()%2!=0) return false;
+
+        stack<char> store;
+        int countPop = 0;
+
+        for(int i=0; i<s.size(); i++){
+
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                store.push(s[i]);
+                continue;
+            }
+
+            if(store.empty()){
+                return false;
+            }
+
+            if(s[i] == ')' && store.top()=='('){
+                store.pop();
+            }
+            else if(s[i] == '}' && store.top()=='{'){
+                store.pop();
+            }
+            else if(s[i] == ']' && store.top()=='['){
+                store.pop();
+            }
+            else{
+                return false;
+            }  
+            countPop++;
+        }
+        if(countPop!=s.size()/2) return false;
+        return true;
+    }
+};
