10402620c6e736bb7f9a7937786832cb14fb1871
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-07-03 18:49:02 -0400

    Time: 17 ms (5.7%), Space: 14.6 MB (96.76%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ //My previous solution (implemented a couple of months ago)
+
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         ListNode *head = new ListNode();
+//         ListNode *hold = new ListNode();
+//         hold->next = head;
+//         //main->next = head;
+//         while(list1!=NULL && list2!=NULL){
+//             if(list1->val<list2->val){
+//                 head->next = list1;
+//                 list1 = list1->next;
+//             } else{
+//                 head->next = list2;
+//                 list2 = list2->next;
+//             }
+//             head = head->next;
+//         }
+//         if(list1!=NULL) head->next = list1;
+//         else if(list2!=NULL) head->next = list2;
+//         if(hold->next->next!=NULL) return hold->next->next;
+//         return NULL;
+//     }
+// };
+
+// Cuttent solution
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        ListNode* newList = new ListNode();
+
+        ListNode* iter;
+        iter = newList; 
+
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val<=list2->val){
+                iter->next = list1;
+                list1 = list1->next;
+            } else {
+                iter->next = list2;
+                list2 = list2->next;
+            }
+            iter = iter->next;
+        }
+        if(list1){
+            iter->next = list1;
+        } else {
+            iter->next = list2;
+        }
+
+        return newList->next;
+    }
+};
