bac122576edf7d1a766d4104e6c6d7c524436c09
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-09-15 21:00:10 -0400

    Time: 1894 ms (88.4%), Space: 0B (100%) - LeetHub

---
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+    p.product_name,
+    s.year,
+    s.price
+FROM Sales s
+INNER JOIN Product p
+    ON s.product_id = p.product_id
+ORDER BY p.product_name
+;
