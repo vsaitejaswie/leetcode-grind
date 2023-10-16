b8eeb30fe4765103531c6ccc05e94e845254b55c
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-10-16 13:01:38 -0400

    Time: 1401 ms (73.4%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -12,3 +12,6 @@ LEFT JOIN UnitsSold u
     ON u.product_id = p.product_id 
     AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
+
+# LEFT JOIN on Prices
+# If I want null value against a column in the joined table, I need to use the condition as part of JOIN statement and not as part of WHERE clause
