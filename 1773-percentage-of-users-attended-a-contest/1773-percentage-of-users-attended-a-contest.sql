85efdce137bd53b54cac8b3108df6dd9cf9f47c4
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-10-16 13:34:20 -0400

    Time: 1513 ms (92.48%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -4,3 +4,6 @@ FROM Register r, Users u
 WHERE r.user_id = u.user_id
 GROUP BY r.contest_id
 ORDER BY percentage DESC, r.contest_id
+
+# I could use DISTINCT too if needed
+# ROUND((COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users)) * 100, 2)
