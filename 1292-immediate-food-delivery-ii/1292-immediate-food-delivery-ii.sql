489032b4039adb5c41bd10690988d07ca9165e92
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-10-16 15:32:49 -0400

    Time: 1026 ms (90.22%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -13,3 +13,18 @@ FROM Delivery, (SELECT
 FROM Delivery
 GROUP BY customer_id) d
 WHERE customer_id = d.cust AND customer_pref_delivery_date = d.pref
+
+
+# ALTERNATE SOLUTION
+
+# WITH FirstOrders AS (
+#     SELECT
+#         customer_id,
+#         MIN(order_date) AS first_order_date
+#     FROM Delivery
+#     GROUP BY customer_id
+# )
+
+# SELECT
+#     ROUND(COUNT(CASE WHEN DATEDIFF(customer_pref_delivery_date, first_order_date) = 0 THEN 1 ELSE NULL END) / COUNT(1) * 100, 2) AS immediate_percentage
+# FROM FirstOrders;
