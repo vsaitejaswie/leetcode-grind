24a5e050c873db90e277b28fb1a1148e937b5335
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-10-19 16:22:55 -0400

    Time: 931 ms (77.54%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -3,3 +3,5 @@ SELECT customer_id
 FROM Customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
+
+# DISTINCT has to be used with product_key as there could be duplicate rows of product_ids
