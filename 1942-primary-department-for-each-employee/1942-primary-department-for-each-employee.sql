30dc8152c1d3adbf0b15c7867f14d53220c12db3
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-10-20 08:06:55 -0400

    Time: 921 ms (94.68%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -7,3 +7,11 @@ SELECT employee_id, department_id
 FROM Employee
 GROUP BY employee_id
 HAVING COUNT(*) = 1
+
+# learn which is more optimal
+# the above query or 
+
+# select employee_id, department_id
+# from Employee
+# where primary_flag = 'Y' or employee_id in (
+# select employee_id from employee group by employee_id having count(department_id) = 1)
