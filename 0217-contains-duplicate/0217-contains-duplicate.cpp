c2bbaa53333764013b1b99b45054366458b41826
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2023-11-25 05:51:53 -0500

    Time: 116 ms (26.08%), Space: 71.1 MB (26.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,9 +1,23 @@
+// Map implementation
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         map<int, int> hstore;
+//         for(int i=0; i<nums.size(); i++)
+//         if(hstore.count(nums[i]) == 0) hstore[nums[i]] = 1;
+//         else return true;
+//         return false;
+//     }
+// };
+
+// Set implementation
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> hstore;
+        set<int> hstore;
         for(int i=0; i<nums.size(); i++)
-        if(hstore.count(nums[i]) == 0) hstore[nums[i]] = 1;
+        if(hstore.count(nums[i]) == 0) hstore.insert(nums[i]);
         else return true;
         return false;
     }
