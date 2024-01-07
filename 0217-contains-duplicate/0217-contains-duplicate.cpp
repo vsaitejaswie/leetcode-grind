94b31ff9935d7e07ea211634b58110c2061151e9
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2024-01-07 15:38:41 -0500

    Time: 107 ms (47.54%), Space: 69.8 MB (67.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -11,11 +11,23 @@
 //     }
 // };
 
-// Set implementation
+// // Set implementation
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         set<int> hstore;
+//         for(int i=0; i<nums.size(); i++)
+//         if(hstore.count(nums[i]) == 0) hstore.insert(nums[i]);
+//         else return true;
+//         return false;
+//     }
+// };
+
+// Unordered Set implementation - ir has faster retrieval rate comlared to set
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> hstore;
+        unordered_set<int> hstore;
         for(int i=0; i<nums.size(); i++)
         if(hstore.count(nums[i]) == 0) hstore.insert(nums[i]);
         else return true;
