7bb291d65bdc77485d4cc6cceee4f291967784db
Veeramreddy Sai Tejaswi <25884668+vsaitejaswie@users.noreply.github.com>
2024-01-24 22:59:00 -0500

    Time: 39 ms (15.14%), Space: 13.9 MB (11.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,58 @@
+// Solved it a few months ago
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         if(s.size() == 0) return 0;
+//         unsigned long maxi = 1;
+//         map<char, int> track;
+//         int point = 0;
+//         int slide = 0;
+
+//         while(slide < s.size()){
+//             if(track.count(s[slide]) == 0){
+//                 track[s[slide]] = 1;
+//                 slide++;
+//             }
+//             else{
+//                 track.erase(s[point]);
+//                 point++;
+//             }
+//             maxi = max(maxi, track.size());
+
+//             if(point == s.size()) break;
+//         }
+//         return maxi;
+//     }
+// };
+
+// // while loop has been used because it doesn't make sense to use for loop
+// // for sliding window implementation for this problem (window size is variable)
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.size() == 0) return 0;
-        unsigned long maxi = 1;
-        map<char, int> track;
-        int point = 0;
-        int slide = 0;
+        int n = s.size();
+        if(n==0 || n==1) return n;
 
-        while(slide < s.size()){
-            if(track.count(s[slide]) == 0){
-                track[s[slide]] = 1;
-                slide++;
-            }
-            else{
-                track.erase(s[point]);
-                point++;
-            }
-            maxi = max(maxi, track.size());
+        int result = 0;
+        unordered_map<char, int> track;
+        int f=0, r=1;
 
-            if(point == s.size()) break;
+        track[s[f]] = f;
+
+        while(r<n) {
+            if(track.find(s[r]) != track.end()) {
+                for(int i=f; i<=track[s[r]]; i++){
+                    cout<<s[i]<<endl;
+                    track.erase(s[i]);
+                    f++;
+                }
+            }
+            track[s[r]] = r;
+            result = max(result, int(track.size()));
+            r++;
         }
-        return maxi;
+        return result;
     }
 };
-
-// while loop has been used because it doesn't make sense to use for loop
-// for sliding window implementation for this problem
